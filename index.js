0b5b28718949b4abfcc642702d53a28f41fc72f7
zhangke <keke2004victory@163.com>
2024-06-12 14:02:22 +0800

    Complete XML escape of variable content

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const default_opt = {
     delimiter: ["{{", "}}"],
     loop_stack: []
 }
+function escapeXml(unsafe) {
+    return unsafe.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+        }
+    });
+}
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     //$&表示整个被匹配的字符串
@@ -53,6 +64,7 @@ let paragraph_arr_process = function (paragraph_arr, data, opt, func) {
     }
     for (let paragraph_obj of paragraph_arr) {
         paragraph_process(paragraph_obj, data, opt, func);
+
     }
 }
 
@@ -69,17 +81,41 @@ let run_arr_process = function (run_arr, data, opt, func) {
 
     return func(run_arr, data, opt);
 }
+// 检查每一个run，查看里面是否需要分段
+let paragraph_split = function (run_arr, data, opt) {
+    let new_run_arr = [];
+    for (let index = 0; index < run_arr.length; ++ index) {
+        if (!run_arr[index]["w:t"][0].hasOwnProperty("_")) {
+            new_run_arr.push(run_arr[index]);
+            continue;
+        }
 
-let run_process = function (run_arr, data, opt) {
-    for (let run_obj of run_arr) {
-        if (run_obj.hasOwnProperty("w:t") && typeof run_obj["w:t"][0] !== "object") {
-            run_obj["w:t"][0] = text_process(run_obj["w:t"][0], data, opt, func);
+        let matched_arr = run_arr[index]["w:t"][0]["_"].match(/(?<=&lt;p&gt;).*?(?=&lt;\/p&gt;)/g);
+        if (!matched_arr) {
+            new_run_arr.push(run_arr[index]);
+            continue;
+        }
+        for (let matched of matched_arr) {
+            new_run_arr.push({
+                "w:t": matched,
+                "w:rFonts": run_arr[index]["w:rFonts"]
+            });
         }
     }
-    return run_arr;
+    return [new_run_arr, null];
 }
 
-// 合并属性一样的run，产生新的数组
+
+// let run_process = function (run_arr, data, opt) {
+//     for (let run_obj of run_arr) {
+//         if (run_obj.hasOwnProperty("w:t") && typeof run_obj["w:t"][0] !== "object") {
+//             run_obj["w:t"][0] = text_process(run_obj["w:t"][0], data, opt, func);
+//         }
+//     }
+//     return run_arr;
+// }
+
+// 检查每一个run，合并属性一样的run，产生新的数组
 let merge_runs_by_rPr = function (run_arr, data, opt) {
     let new_run_arr = [run_arr[0]];
     for (let index = 1; index < run_arr.length; ++ index) {
@@ -109,39 +145,47 @@ let cmp_run_rPr = function (run_a, run_b) {
 }
 
 let cmp_rPr = function (rPr_a_arr, rPr_b_arr) {
-    if(util.isDeepStrictEqual(rPr_a_arr, rPr_b_arr)) {
-        return true;
-    }
-
     if (rPr_a_arr === undefined || rPr_b_arr === undefined) {
-        return false;
+        return true;
     }
 
     let rPr_a = rPr_a_arr[0];
     let rPr_b = rPr_b_arr[0];
-    if (rPr_a.hasOwnProperty("w:rFonts") + rPr_b.hasOwnProperty("w:rFonts") == 1) {
-        return false;
-    }
+    // if (rPr_a.hasOwnProperty("w:rFonts") + rPr_b.hasOwnProperty("w:rFonts") == 1) {
+    //     return false;
+    // }
 
+    if (rPr_a.hasOwnProperty("w:rFonts") && rPr_a["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
+        delete rPr_a["w:rFonts"][0]["$"]["w:hint"];
+        if (Object.keys(rPr_a["w:rFonts"][0]["$"]).length === 0) {
+            delete rPr_a["w:rFonts"];
+        }
+    }
+    if (rPr_b.hasOwnProperty("w:rFonts") && rPr_b["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
+        delete rPr_b["w:rFonts"][0]["$"]["w:hint"];
+        if (Object.keys(rPr_b["w:rFonts"][0]["$"]).length === 0) {
+            delete rPr_b["w:rFonts"];
+        }
+    }
     // w:rPr里可能没有w:rFonts。只针对两者都有的话，忽略hint值使之相同。
-    if (rPr_a.hasOwnProperty("w:rFonts") + rPr_b.hasOwnProperty("w:rFonts") == 2) {
-        if (!rPr_a["w:rFonts"][0]["$"].hasOwnProperty("w:hint") && rPr_b["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
-            rPr_a["w:rFonts"][0]["$"]["w:hint"] = rPr_b["w:rFonts"][0]["$"]["w:hint"];
-        }
+    // if (rPr_a.hasOwnProperty("w:rFonts") + rPr_b.hasOwnProperty("w:rFonts") == 2) {
+    //     if (!rPr_a["w:rFonts"][0]["$"].hasOwnProperty("w:hint") && rPr_b["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
+    //         rPr_a["w:rFonts"][0]["$"]["w:hint"] = rPr_b["w:rFonts"][0]["$"]["w:hint"];
+    //     }
+    //
+    //     if (!rPr_b["w:rFonts"][0]["$"].hasOwnProperty("w:hint") && rPr_a["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
+    //         rPr_b["w:rFonts"][0]["$"]["w:hint"] = rPr_a["w:rFonts"][0]["$"]["w:hint"];
+    //     }
+    // }
 
-        if (!rPr_b["w:rFonts"][0]["$"].hasOwnProperty("w:hint") && rPr_a["w:rFonts"][0]["$"].hasOwnProperty("w:hint")) {
-            rPr_b["w:rFonts"][0]["$"]["w:hint"] = rPr_a["w:rFonts"][0]["$"]["w:hint"];
-        }
+    if (rPr_a.hasOwnProperty("w:lang")) {
+        delete rPr_a["w:lang"];
+    }
+    if (rPr_b.hasOwnProperty("w:lang")) {
+        delete rPr_b["w:lang"];
     }
 
-    if (rPr_a.hasOwnProperty("w:lang") && !rPr_b.hasOwnProperty("w:lang")) {
-        rPr_b["w:lang"] = rPr_a["w:lang"];
-    }
-    if (rPr_b.hasOwnProperty("w:lang") && !rPr_a.hasOwnProperty("w:lang")) {
-        rPr_a["w:lang"] = rPr_b["w:lang"];
-    }
-
-    let result = util.isDeepStrictEqual(rPr_a, rPr_b);
+    let result = util.isDeepStrictEqual(rPr_a_arr, rPr_b_arr);
     return result;
 }
 
@@ -177,12 +221,12 @@ let replace_hold = function (template, data, opt) {
         if (!data.hasOwnProperty(variable_name)) {
             continue;
         }
-        template = template.replaceAll(text_matched, data[variable_name]);
+        template = template.replaceAll(text_matched, escapeXml(data[variable_name]));
     }
     return template;
 }
 
-let extend_loop = function (template, data, opt) {
+let replace_with_extend = function (template, data, opt) {
     if (template === undefined || template === "") {
         return template;
     }
@@ -192,7 +236,6 @@ let extend_loop = function (template, data, opt) {
     if (!loop_matched_arr) {
         return replace_hold(template, data, opt);
     }
-    let result = "";
     let loop_matched = loop_matched_arr[0];
     let left_str = template.slice(0, template.indexOf(loop_matched));
     let right_str = template.substring(template.indexOf(loop_matched) + loop_matched.length);
@@ -203,16 +246,50 @@ let extend_loop = function (template, data, opt) {
     right_str = right_str.slice(pos + delimiter_tmp.length);
 
     left_str = replace_hold(left_str, data, opt);
-    right_str = extend_loop(right_str, data, opt);
+    right_str = replace_with_extend(right_str, data, opt);
     let middle_result = "";
     if (data.hasOwnProperty(loop_variable_name)) {
         let count = data[loop_variable_name].length
         for (let index = 0; index < count; ++ index) {
-            middle_result += extend_loop(middle_str, data[loop_variable_name][index], opt);
+            middle_result += replace_with_extend(middle_str, data[loop_variable_name][index], opt);
         }
     }
     return left_str + middle_result + right_str;
 }
+
+// // 利用正则表达式来展开需要分段的部分
+// let paragraph_split = function (template, data, opt) {
+//     if (template === undefined || template === "") {
+//         return template;
+//     }
+//     let reg_str = escapeRegExp('&lt;p&gt;' + "\#.*?" + '&lt;/p&gt;');
+//     let reg = new RegExp(reg_str, "g");
+//     let matched_arr = template.match(reg);
+//     if (!matched_arr) {
+//         return template;
+//     }
+//     for (let matched of matched_arr) {
+//         template.replaceAll(matched, escapeXml(data[variable_name]));
+//     }
+//     let left_str = template.slice(0, template.indexOf(loop_matched));
+//     let right_str = template.substring(template.indexOf(loop_matched) + loop_matched.length);
+//     let loop_variable_name = loop_matched.slice(opt.delimiter[0].length + 1, 0 - opt.delimiter[1].length);
+//     let delimiter_tmp = opt.delimiter[0] + "\/" + loop_variable_name + opt.delimiter[1]
+//     let pos = right_str.indexOf(delimiter_tmp);
+//     let middle_str = right_str.slice(0, pos);
+//     right_str = right_str.slice(pos + delimiter_tmp.length);
+//
+//     left_str = replace_hold(left_str, data, opt);
+//     right_str = replace_with_extend(right_str, data, opt);
+//     let middle_result = "";
+//     if (data.hasOwnProperty(loop_variable_name)) {
+//         let count = data[loop_variable_name].length
+//         for (let index = 0; index < count; ++ index) {
+//             middle_result += replace_with_extend(middle_str, data[loop_variable_name][index], opt);
+//         }
+//     }
+//     return left_str + middle_result + right_str;
+// }
 
 let render_docx = async function (template, data, opt){
     opt = Object.assign(default_opt, opt);
@@ -228,9 +305,7 @@ let render_docx = async function (template, data, opt){
     });
 
     // 先合并相同的run
-    if (document_obj.hasOwnProperty("w:document") && document_obj["w:document"].hasOwnProperty("w:body")) {
-        body_arr_process(document_obj["w:document"]["w:body"], data, opt, merge_runs_by_rPr);
-    }
+    body_arr_process(document_obj["w:document"]["w:body"], data, opt, merge_runs_by_rPr);
 
     // 再还原出xml
     let xml2js_builder = new xml2js.Builder({
@@ -240,9 +315,11 @@ let render_docx = async function (template, data, opt){
     });
     document_xml = xml2js_builder.buildObject(document_obj);
 
-    //
-    data = Object.assign(data, flat(data));
-    document_xml = extend_loop(document_xml, data, opt);
+    // 利用正则表达式来展开循环，替换placeholder
+    document_xml = replace_with_extend(document_xml, data, opt);
+
+    // 利用正则表达式来展开需要分段的部分
+    // body_arr_process(document_obj["w:document"]["w:body"], data, opt, paragraph_split);
 
     // download image
     // let [err, buf_image] = await utils.download("https://pics1.baidu.com/feed/9d82d158ccbf6c8176395c9eed85af3b32fa4087.jpeg");
